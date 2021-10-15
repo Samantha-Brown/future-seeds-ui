86b651d0682c1bb502f04994f013dabc840ccc43
David Leach <davidleach724@gmail.com>
2021-10-15 15:12:54 -0600

    Create routes for login and signup

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,8 @@
+import Home from '../Home/Home';
 import { Route, Switch } from 'react-router'
 import './App.css';
+import SignUp from '../SignUp/SignUp';
+import LogIn from '../LogIn/LogIn';
 
 const App = () => {
 
@@ -7,6 +10,8 @@ const App = () => {
     <main>
       <Switch>
         <Route exact path='/' component={ Home } />
+        <Route exact path='/signup/' component= { SignUp }/>
+        <Route exact path='/login/' component={ LogIn } />
       </Switch>
     </main>
   )
