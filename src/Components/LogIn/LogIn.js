12a8a50ab7306019a62f5b8afd461400c5174878
David Leach <davidleach724@gmail.com>
2021-10-16 13:17:55 -0600

    Create dropdown with concat names

--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -1,9 +1,24 @@
+import { users } from '../../data/mockData'
 import './LogIn.css'
+
+import loginImg from '../../images/login.png'
+import signUpImg from '../../images/signup.png'
 
 const LogIn = () => {
 
+  const userList = users.map(user => {
+    return (
+      <option value={user.id} key={user.id}>{`${user.first_name}-${user.last_name}-${user.id.toString()}`}</option>
+    )
+  })
+
   return (
-    <h1>Log In</h1>
+    <div className='login-container'>
+      <h3>Select Your User Name</h3>
+      <select className='user-list'> {userList} </select>
+      <img src={loginImg} className='menu-btn' alt='log in btn'/>
+      <h4>New User? Sign Up Here</h4>
+    </div>
   )
 }
 
--- src/data/mockData.js
+++ src/data/mockData.js
@@ -1,6 +1,6 @@
 export const users = [
   {
-    id: 01,
+    id: 1,
     first_name: 'Daisy',
     last_name: 'Lee',
     city: 'Denver',
@@ -8,7 +8,7 @@ export const users = [
     intentions: 'To impress all of the neighbors'
   },
   {
-    id: 02,
+    id: 2,
     first_name: 'Rose',
     last_name: 'ODanald',
     city: 'Tampa',
@@ -16,7 +16,7 @@ export const users = [
     intentions: 'To eat fresh vegatables'
   },
   {
-    id: 03,
+    id: 3,
     first_name: 'Charles',
     last_name: 'Tomato',
     city: 'Portland',
@@ -27,7 +27,7 @@ export const users = [
 
 export const seeds = [
   {
-    id: 01,
+    id: 1,
     name: 'Blue Cornflower',
     planting_depth: '2',
     days_to_germinate: '5-10',
@@ -36,10 +36,10 @@ export const seeds = [
     sun_exposure:'full',
     expected_plant_height:'4ft',
     notes:'Planted them in the under the window',
-    user_id: 01
+    user_id: 1
   },
   {
-    id: 02,
+    id: 2,
     name: 'Green Beans',
     planting_depth: '2',
     days_to_germinate: '10-15',
@@ -48,10 +48,10 @@ export const seeds = [
     sun_exposure:'full',
     expected_plant_height:'1ft',
     notes:'Planted next to tomatoes for nitrogen',
-    user_id: 02
+    user_id: 2
   },
   {
-    id: 03,
+    id: 3,
     name: 'Patty Pan Squash',
     planting_depth: '.25',
     days_to_germinate: '5-10',
@@ -60,10 +60,10 @@ export const seeds = [
     sun_exposure:'full',
     expected_plant_height:'1ft',
     notes:'I planted the yellow ones!',
-    user_id: 03
+    user_id: 3
   },
   {
-    id: 04,
+    id: 4,
     name: 'Kale',
     planting_depth: '.25',
     days_to_germinate: '7/10',
@@ -72,27 +72,27 @@ export const seeds = [
     sun_exposure:'partial',
     expected_plant_height:'1ft',
     notes:'I hope the rabbits don\'t get them this year',
-    user_id: 02
+    user_id: 2
   }
 ]
 
 export const journalEntries = [
     {
-      id: 01,
+      id: 1,
       date: '10/17/2021',
       description: `I love possums so much, and today is national possum day. I planted my garden to share with all the possums :) I'm baby intelligentsia la croix mixtape slow-carb vinyl, kogi +1 shabby chic waistcoat blue bottle irony letterpress pork belly. Asymmetrical whatever beard iPhone VHS woke unicorn gastropub. Cold-pressed biodiesel shaman butcher literally, distillery normcore marfa pinterest health goth. Tumblr hammock distillery kombucha, taiyaki knausgaard small batch cliche affogato VHS.`,
-      user_id: 03
+      user_id: 3
     },
     {
-      id: 02,
+      id: 2,
       date: '10/22/2021',
       description: `Today was the first freeze of winter. Good bye garden see you in the Spring. RIP. Sriracha actually tousled, echo park hoodie whatever man braid williamsburg 90's DIY. Put a bird on it gluten-free typewriter deep v. Brooklyn cronut blog drinking vinegar skateboard wolf raclette. Pop-up locavore kale chips gluten-free venmo tilde meggings paleo kinfolk squid. XOXO affogato jianbing chillwave. Activated charcoal swag live-edge disrupt small batch single-origin coffee irony.`,
-      user_id: 01
+      user_id: 1
     },
     {
-      id: 03,
+      id: 3,
       date: '04/22/2022',
       description: `Happy Earth day! Excited to be planting my new apple tree :) Cornhole fixie salvia, kogi ramps retro next level whatever cold-pressed skateboard sustainable edison bulb cray vape. Coloring book marfa biodiesel hashtag bespoke tousled literally vice hexagon narwhal gochujang synth you probably haven't heard of them. Pok pok craft beer austin iceland small batch hell of. Etsy crucifix cornhole palo santo helvetica, sartorial aesthetic chia distillery cliche mlkshk health goth fixie edison bulb`,
-      user_id: 03
+      user_id: 3
     }
 ]
