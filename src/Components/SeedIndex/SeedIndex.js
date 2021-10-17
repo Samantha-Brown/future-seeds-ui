3ccfaba6d8f719f23ee87bd35590bcf740f1bf1c
Hayley Dixon <hdixon37@gmail.com>
2021-10-17 14:45:51 -0600

    Initial set up of component, connect with state change

---
+++ src/Components/SeedIndex/SeedIndex.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+
+import './SeedIndex.css'
+
+const SeedIndex = ( {currentUser}) => {
+  console.log(currentUser)
+  console.log('? ')
+
+  return (
+    <div className='seed-container'>
+      <div>I forgot how to do anything </div>
+    </div>
+  )
+}
+
+export default SeedIndex;
