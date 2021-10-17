f97a05e0d31e832f7d8702d3cfdad8104c9d7bff
David Leach <davidleach724@gmail.com>
2021-10-17 11:37:40 -0600

    Add all user input fields

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -3,10 +3,11 @@ import StateList from '../StateList/StateList'
 import './SignUp.css'
 
 const SignUp = () => {
-  const [userState, setUserState] = useState('AL')
   const [userFirstName, setUserFirstName] = useState('')
   const [userLastName, setUserLastName] = useState('')
   const [userCity, setUserCity] = useState('')
+  const [userState, setUserState] = useState('AL')
+  const [userIntentions, setUserIntentions] = useState('')
 
   const handleLocationChange = (location) => {
     setUserState(location)
@@ -15,25 +16,33 @@ const SignUp = () => {
 
   return (
     <div className='signup-container'>
-      <input
-        type='text'
-        placeholder='First Name'
-        name='firstName'
-        onChange={e => setUserFirstName(e.target.value)}
-      />
-      <input
-        type='text'
-        placeholder='Last Name'
-        name='lastName'
-        onChange={e => setUserLastName(e.target.value)}
-      />
-      <input
-        type='text'
-        placeholder='City'
-        name='city'
-        onChange={e => setUserCity(e.target.value)}
-      />
-      <StateList handleLocationChange={handleLocationChange}/>
+      <div className='input-container'>
+        <input
+          type='text'
+          placeholder='First Name'
+          name='firstName'
+          onChange={e => setUserFirstName(e.target.value)}
+        />
+        <input
+          type='text'
+          placeholder='Last Name'
+          name='lastName'
+          onChange={e => setUserLastName(e.target.value)}
+        />
+        <input
+          type='text'
+          placeholder='City'
+          name='city'
+          onChange={e => setUserCity(e.target.value)}
+        />
+        <StateList handleLocationChange={handleLocationChange}/>
+        <input
+          type='text'
+          placeholder='Intentions (short blurb)'
+          name='intentions'
+          onChange={e => setUserIntentions(e.target.value)}
+        />
+      </div>
     </div>
   )
 }
