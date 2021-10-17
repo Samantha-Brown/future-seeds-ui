8ff3b28fcce2a2298e6f1442bf3ff983f563cc96
Hayley Dixon <hdixon37@gmail.com>
2021-10-17 14:42:27 -0600

    Add State to App for Route to SeedIndex

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,15 +3,33 @@ import { Route, Switch } from 'react-router'
 import './App.css';
 import SignUp from '../SignUp/SignUp';
 import LogIn from '../LogIn/LogIn';
+import SeedIndex from '../SeedIndex/SeedIndex'
+
+import { useEffect, useState } from 'react'
 
 const App = () => {
+  const [currentUser, setCurrentUser] = useState('')
+
+  const handleChange = (userID) => {
+    setCurrentUser(userID)
+  }
 
   return (
     <main>
       <Switch>
         <Route exact path='/' component={ Home } />
         <Route exact path='/signup/' component= { SignUp }/>
-        <Route exact path='/login/' component={ LogIn } />
+        <Route exact path='/login/'>
+          <LogIn
+            currentUser={ currentUser }
+            handleChange = { handleChange }
+            />
+          </Route>
+        <Route exact path='/seeds'>
+          <SeedIndex
+            currentUser={ currentUser }
+            />
+        </Route>
       </Switch>
     </main>
   )
