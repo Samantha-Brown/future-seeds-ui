b5a982da3300569112f2ce4f06fceb6b596cfa41
Samantha Brown <bro7445@gmail.com>
2021-10-18 15:23:17 -0400

    Add the imports and export of Journal index

---
+++ src/Components/JournalIndex/JournalIndex.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { users, seeds } from '../../data/mockData'
+import JournalCard from '../JournalCard/JournalCard'
+import './JournalIndex.css'
+
+
+
+
+
+
+
+export default JournalIndex
