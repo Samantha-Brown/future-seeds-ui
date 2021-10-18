e1101cdec7ba52544b599d6a3e71bec08b2a75c2
Hayley Dixon <hdixon37@gmail.com>
2021-10-18 12:34:29 -0600

    Update form to use useForm Hooks

--- src/Components/SeedForm/SeedForm.js
+++ src/Components/SeedForm/SeedForm.js
@@ -1,54 +1,29 @@
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import { useForm } from "react-hook-form";
 import { users, seeds } from '../../data/mockData'
 import SeedCard from '../SeedCard/SeedCard'
 import './SeedForm.css'
 
 const SeedForm = () => {
-const [sunExposure, setSunExposure] = useState('')
+const { register, handleSubmit, watch, formState: { errors } } = useForm();
+const onSubmit = data => console.log(data)
 
   return (
     <div className='seed-form'>
-      <form>
-        <input
-          type='text'
-          placeholder='Seed Name'
-          name='name'
-          />
-        <input
-          type='text'
-          placeholder='Planting Depth'
-          name='planting_depth'
-          />
-        <input
-          type='text'
-          placeholder='Days to Germinate'
-          name='days_to_germinate'
-          />
-        <input
-          type='text'
-          placeholder='Plant Height'
-          name='expected_plant_height'
-          />
-        <select>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input placeholder='Seed Name' {...register('name')} />
+        <input placeholder='Planting Depth' {...register('planting_depth')} />
+        <input placeholder='Days to Germinate' {...register('days_to_germinate')} />
+        <input placeholder='Plant Height' {...register('expected_plant_height')} />
+        <select {...register('sun_exposure')}>
           <option value=''>Select...</option>
           <option value='full_sun'>Full Sun</option>
           <option value='partial_sun'>Partial Sun</option>
           <option value='full_shade'>Full Shade</option>
-          type='text'
-          placeholder='Sun Exposure'
-          name='sun_exposure'
         </select>
-        <input
-          type='text'
-          placeholder='Time to Harvest'
-          name='time_to_harvest'
-          />
-        <input
-          type='text'
-          placeholder='Notes'
-          name='notes'
-          />
+        <input placeholder='Time to Harvest' {...register('time_to_harvest')} />
+        <input placeholder='Notes' {...register('notes')} />
         <input type='submit' />
       </form>
     </div>
@@ -56,3 +31,26 @@ const [sunExposure, setSunExposure] = useState('')
 }
 
 export default SeedForm;
+
+
+
+// <input
+//   type='text'
+//   placeholder='Seed Name'
+//   name='name'
+//   />
+// <input
+//   type='text'
+//   placeholder='Planting Depth'
+//   name='planting_depth'
+//   />
+// <input
+//   type='text'
+//   placeholder='Days to Germinate'
+//   name='days_to_germinate'
+//   />
+// <input
+//   type='text'
+//   placeholder='Plant Height'
+//   name='expected_plant_height'
+//   />
