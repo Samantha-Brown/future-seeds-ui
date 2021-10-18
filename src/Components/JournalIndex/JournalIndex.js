e639d09d2322b651f409e8385f2a623d8cd1f926
Samantha Brown <bro7445@gmail.com>
2021-10-18 15:40:04 -0400

    Build out Journal index component

--- src/Components/JournalIndex/JournalIndex.js
+++ src/Components/JournalIndex/JournalIndex.js
@@ -1,13 +1,33 @@
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
-import { users, seeds } from '../../data/mockData'
+import { users, journalEntries } from '../../data/mockData'
 import JournalCard from '../JournalCard/JournalCard'
 import './JournalIndex.css'
 
+const JournalIndex = ( { } ) => {
 
+  const userDetails = users.find(user => user.id === Number(currentUser))
 
+  const userJournalEntries = journalEntries.filter(entry => entry.user_id ===Number(currentUser))
 
+  const journalCards = userJournalEntries.map(journalEntries => {
+    return (
+      <div className='journal-card' key={userJournalEntries.id}>
+        <h1>{journalEntries.date}</h1>
+        <p>{journalEntries.description}</p>
+      </div>
+    )
+  })
 
-
+  return (
+    <div className='journal-index'>
+      <div>{`Hello, ${userDetails.first_name}`} </div>
+      <button>Add new entry</button>
+      <JournalCard
+        journalCards={JournalCards}
+        />
+    </div>
+  )
+}
 
 export default JournalIndex
