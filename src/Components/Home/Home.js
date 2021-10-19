cc1c5ea360467b02169161a9c16d41b5ccf6c33f
David Leach <davidleach724@gmail.com>
2021-10-19 15:39:03 -0600

    Add home line bar

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -16,9 +16,11 @@ const Home = () => {
           <Link to='signup' className='links'>Sign Up</Link>
         </div>
       </div>
+      <div className='home-line'></div>
       <div className='main-title'>
         <img src={titleLogo} alt='future seeds title' className='big-logo'/>
       </div>
+      <div className='home-line'></div>
       <div className='mission'>
         <h4>... this is where the impactful mission statement goes. Oh my, it's SO impactful. Seeds, Journals, etc.</h4>
         <p>- Future Seeds</p>
