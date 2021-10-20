7398fe6201801303e6ce8820410c4c1d2611b982
Hayley Dixon <78764587+hheyhhay@users.noreply.github.com>
2021-10-20 13:35:54 -0600

    Feature/styling (#27)
    * Add styling, React form and Cypress

--- src/Components/SeedIndex/SeedIndex.js
+++ src/Components/SeedIndex/SeedIndex.js
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import { users, seeds } from '../../data/mockData'
 import SeedCard from '../SeedCard/SeedCard'
 import SeedForm from '../SeedForm/SeedForm'
+import plantImage from '../../images/caleb-george.jpg'
+
 import './SeedIndex.css'
 
 const SeedIndex = ( {currentUser}) => {
@@ -12,13 +14,22 @@ const SeedIndex = ( {currentUser}) => {
   const seedCards = userSeeds.map(userSeeds => {
     return (
       <div className='seed-card' key={userSeeds.id}>
-        <h1>{userSeeds.name}</h1>
-        <ul>{`Plant ${userSeeds.planting_depth} inches deep`}</ul>
-        <ul>{`${userSeeds.days_to_germinate} days to germinate`}</ul>
-        <ul>{`${userSeeds.expected_plant_height} inches tall`}</ul>
-        <ul>{`${userSeeds.sun_exposure} sun`}</ul>
-        <ul>{`${userSeeds.time_to_harvest} Days to harvest`}</ul>
-        <ul>{` Notes: ${userSeeds.notes}`}</ul>
+        <h1 className='seed-title'>{userSeeds.name}</h1>
+        <div className='noise'></div>
+        <div className='plant-info'>
+          <img className='plant-image' src={plantImage} alt='green plant with pink background' />
+          <div className='plant-text'>
+            <ul>{`Plant ${userSeeds.planting_depth} inches deep`}</ul>
+            <ul>{`${userSeeds.days_to_germinate} days to germinate`}</ul>
+            <ul>{`${userSeeds.expected_plant_height} inches tall`}</ul>
+            <ul>{`${userSeeds.sun_exposure} sun`}</ul>
+            <ul>{`${userSeeds.time_to_harvest} Days to harvest`}</ul>
+            <div className='notes'>
+              <ul>Notes:</ul>
+              <ul>{userSeeds.notes}</ul>
+            </div>
+          </div>
+        </div>
       </div>
     )
   })
