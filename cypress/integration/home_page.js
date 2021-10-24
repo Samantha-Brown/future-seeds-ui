ff6f4ce31a320207fdfbd68a0d2815db184729b7
Samantha Brown <bro7445@gmail.com>
2021-10-24 16:17:29 -0400

    Add intial cypress fixtures, commands, and tests

--- cypress/integration/home_page.js
+++ cypress/integration/home_page.js
@@ -1,3 +1,23 @@
-describe('User flows via home page', () => {
-  
+describe('Testing stubbing', () => {
+  beforeEach(() => {
+    cy.loadUser()
+ });
+
+ it('should go to localhost', () => {
+    cy.url().should('eq', 'http://localhost:3000/')
+  });
+
+ it('Should confirm that true is equal to true', () => {
+       expect(true).to.equal(true)
+     });
+
+  it('Should show the title', () => {
+    cy.get('.home-container')
+  });
+
+  it('Should show the logo', () => {
+    cy.loadUser()
+    cy.get('.small-logo')
+  })
+
 })
