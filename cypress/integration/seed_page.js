b26147d22f188a6b76d496facc5a4eb4532f39db
David Leach <davidleach724@gmail.com>
2021-10-24 15:58:01 -0600

    Load user seeds

--- cypress/integration/seed_page.js
+++ cypress/integration/seed_page.js
@@ -1,14 +1,6 @@
 describe('Testing stubbing', () => {
-  // beforeEach(() => {
-  //   cy.loadSeeds()
-  // });
-
-  // it('should go to localhost', () => {
-  //   cy.loadSeeds()
-  //   cy.url().should('eq', 'http://localhost:3000/seeds');
-  // });
 
   it('should choose user and load seeds', () => {
-    cy.chooseUser()
+    cy.loadSeeds()
   })
 });
