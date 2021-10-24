d104f9d9b9907cf7cc98b9d98bc0987e6e4d8e8e
Samantha Brown <bro7445@gmail.com>
2021-10-24 16:17:29 -0400

    Add intial cypress fixtures, commands, and tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,16 @@
 Cypress.Commands.add('loadApp', () => {
-  cy.visit('http.localhost:3000')
+  cy.visit('http://localhost:3000')
+})
+
+//stubbing
+Cypress.Commands.add('interceptAPI', (fixturePage, url) => {
+  cy.intercept(`${url}`, {
+    fixture: `user_profile.json`
+  })
+})
+
+//endpoint
+Cypress.Commands.add('loadUser', () => {
+  cy.loadApp()
+    .interceptAPI('users', 'https://future-seeds-api.herokuapp.com/graphql')
 })
