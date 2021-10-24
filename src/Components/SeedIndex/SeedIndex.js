a402e28c1aae7e9972cef2a0be28e2c3a2e6551f
Hayley Dixon <hdixon37@gmail.com>
2021-10-24 11:49:00 -0600

    Refactoring and styling

--- src/Components/SeedIndex/SeedIndex.js
+++ src/Components/SeedIndex/SeedIndex.js
@@ -8,40 +8,32 @@ import { useQuery } from '@apollo/client'
 import { LOAD_SELECTED_USER } from '../../GraphQL/Queries'
 import './SeedIndex.css'
 
-const SeedIndex = ( {currentUserID}) => {
+const SeedIndex = ({ currentUserID }) => {
   const [showForm, setShowForm] = useState(false)
-  // const userDetails = users.find(user => user.id === Number(currentUser))
-  // const userSeeds = seeds.filter(seed => seed.user_id === Number(currentUser))
-
   const [userSeeds, setUserSeeds] = useState(null)
-  const { error2, loading2, data } = useQuery(
-    LOAD_SELECTED_USER,
-    {
-      variables: {id: currentUserID}
-    }
-  )
+  const { error, loading, data } = useQuery(
+    LOAD_SELECTED_USER, { variables: {id: currentUserID} });
 
   useEffect(() => {
     if (data) {
       setUserSeeds(data.user.seeds)
     }
-  })
-  console.log(userSeeds)
+  });
 
   return (
     <div>
     <NavBar/>
     <div className='seed-index'>
-      <button onClick= {() => setShowForm(true)}>New Card</button>
-      {showForm && <div>
+      <button onClick={() => setShowForm(true)}>New Card</button>
+      { showForm && <div>
         <SeedForm />
-        </div>}
+        </div> }
       { userSeeds && <SeedCard
         userSeeds={ userSeeds }
         /> }
     </div>
     </div>
   )
-}
+};
 
 export default SeedIndex;
