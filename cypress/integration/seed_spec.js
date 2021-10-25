d93ae9627dfc5757da60d0d61e45f1406ac77e3f
Hayley Dixon <hdixon37@gmail.com>
2021-10-25 13:54:11 -0600

    Write tests for seed page

--- cypress/integration/seed_spec.js
+++ cypress/integration/seed_spec.js
@@ -18,8 +18,33 @@ beforeEach(() => {
   });
 
   })
-  it('Should go to Seeds Page', () => {
+  it.skip('Should go to Seeds Page', () => {
+    cy.url().should('eq', 'http://localhost:3000/seeds')
+  })
+  it.skip('Should have a nav bar', () => {
+    cy.get('.top-home-bar')
+      .should('have.text', 'SeedsJournalLog Out')
+  })
+  it.skip('Should display user\'s seed cards', () => {
+    cy.get('.seed-card')
+      .should('have.length', 3)
+  })
 
-    expect(true).to.equal(true)
+  it.skip('Should display seed name', () => {
+    cy.get('.seed-title')
+      .first()
+      .should('have.text', 'Pink carrot')
+  })
+
+  it.skip('Should display seed information', () => {
+    cy.get(':nth-child(1) > .plant-info > .plant-text > :nth-child(1)')
+      .should('have.text', 'Plant 1/8 in inches deep')
+  })
+
+  it.skip('Should be able to click button and show new seed form', () => {
+    cy.get('button')
+      .click()
+    cy.get('.seed-form-container')
+      .should('be.visible')
   })
 })
