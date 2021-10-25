95556f1d870f7d1a79304f8e6658b04027d636ac
Samantha Brown <bro7445@gmail.com>
2021-10-25 17:03:49 -0400

    Center and set all cards to the same colors.

--- src/Components/JournalCard/JournalCard.js
+++ src/Components/JournalCard/JournalCard.js
@@ -9,9 +9,11 @@ const JournalCard = ( { userJournals } ) => {
 
   const journalCards = userJournals.map(journalEntries => {
     return (
+      <div className='all-journal-cards'>
       <div className='journal-card' key={userJournals.id}>
         <h1>{journalEntries.date}</h1>
         <p>{journalEntries.description}</p>
+      </div>
       </div>
     )
   })
