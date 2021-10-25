ba2714a2121d3221c4ee23a39289b0632cc6c33a
Hayley Dixon <hdixon37@gmail.com>
2021-10-25 12:59:40 -0600

    Add query names to queries, remove unneeded fields

--- src/GraphQL/Queries.js
+++ src/GraphQL/Queries.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const LOAD_ALL_USERS = gql`
-  {
+  query getUsers {
     users {
       firstName
       lastName
@@ -11,7 +11,7 @@ export const LOAD_ALL_USERS = gql`
 `
 
 export const LOAD_SELECTED_USER = gql`
-  query ($id: ID!) {
+  query getSelectedUser ($id: ID!) {
     user (id: $id) {
       firstName
       lastName
@@ -19,7 +19,6 @@ export const LOAD_SELECTED_USER = gql`
       journalEntries {
         description
         date
-        id
       }
       seeds {
         name
@@ -29,7 +28,6 @@ export const LOAD_SELECTED_USER = gql`
         datePlanted
         expectedPlantHeight
         notes
-        userId
       }
     }
   }
