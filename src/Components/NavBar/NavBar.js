e9bb23445c6ecdf0fd5f20a69402b1f5c1b4a4cb
Hayley Dixon <hdixon37@gmail.com>
2021-10-25 13:37:11 -0600

    Add classNames for Cypress testing

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -9,9 +9,9 @@ return (
     <div className='top-home-bar'>
       <img src={boxLogo} alt='small logo' className='small-logo'/>
       <div className='home-links'>
-        <Link to='/seeds' className='links' style={{ textDecoration: 'none' }}>Seeds</Link>
-        <Link to='/journal' className='links' style={{ textDecoration: 'none' }}>Journal</Link>
-        <Link to='/' className='links' style={{ textDecoration: 'none' }}>Log Out</Link>
+        <Link to='/seeds' className='links to-seeds' style={{ textDecoration: 'none' }}>Seeds</Link>
+        <Link to='/journal' className='links to-journal' style={{ textDecoration: 'none' }}>Journal</Link>
+        <Link to='/' className='links to-home' style={{ textDecoration: 'none' }}>Log Out</Link>
       </div>
     </div>
     <div className='home-line'></div>
