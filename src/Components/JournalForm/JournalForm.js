ecbe8ef5ac1e72ec151a6d626d6f622f9eeca49a
Hayley Dixon <hdixon37@gmail.com>
2021-10-25 18:58:10 -0600

    Fix spacing

--- src/Components/JournalForm/JournalForm.js
+++ src/Components/JournalForm/JournalForm.js
@@ -3,13 +3,12 @@ import './JournalForm.css'
 
 const JournalForm = () => {
 const { register, handleSubmit, watch, formState: { errors } } = useForm();
-const onSubmit = data => console.log(data)
 
   return (
     <div className='journal-form-container'>
-      <form className='journal-form' onSubmit={handleSubmit(onSubmit)}>
-        <input placeholder='Date' className='input-date' {...register('date')} />
-        <input placeholder='Description' className='input-description' {...register('description')} />
+      <form className='journal-form' onSubmit={ handleSubmit(onSubmit) }>
+        <input placeholder='Date' className='input-date' { ...register('date') } />
+        <input placeholder='Description' className='input-description' { ...register('description') } />
         <input type='submit' className='entry-btn' />
       </form>
     </div>
