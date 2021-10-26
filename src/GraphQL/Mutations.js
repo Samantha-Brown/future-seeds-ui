706132e8d72b4bc4cfdc1e110149362688c76767
Samantha Brown <79541611+Samantha-Brown@users.noreply.github.com>
2021-10-26 18:45:28 -0400

    Include the create new seed mutation

--- src/GraphQL/Mutations.js
+++ src/GraphQL/Mutations.js
@@ -25,5 +25,38 @@ export const CREATE_USER_MUTATION = gql`
   }
 `
 
+export const CREATE_SEED_MUTATION = gql`
+mutation (
+    $name: String!
+    $plantingDepth: String!
+    $timeToHarvest: String!
+    $datePlanted: Sting!
+    $expectedPlantHeight: String!
+    $notes: String!
+    $sunExposure: String!
+    $userId: ID!
+  ){
+    createSeedEntry(input {
+        name: $name
+        plantingDepth: $plantingDepth
+        timeToHarvest: $timeToHarvest
+        datePlanted: $datePlanted
+        expectedPlantHeight: $expectedPlantHeight
+        notes: $notes
+        sunExposure: $sunExposure
+        userId: $userID
+    )}
+    {
+      name
+      plantingDepth
+      timeToHarvest
+      datePlanted
+      expectedPlantHeight
+      notes
+      sunExposure
+      userId
+    }
+  }
+`
 // line 19-23 is the spot what happens after the mutation
 // what is returned
