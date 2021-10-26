ae467faed692a275b940ac8cfc218c232117dd17
David Leach <davidleach724@gmail.com>
2021-10-26 08:34:12 -0600

    Refactor Home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,34 +1,46 @@
-import { Link } from 'react-router-dom'
-
-import boxLogo from '../../images/fs.png'
-import titleLogo from '../../images/title-green.png'
-
-import './Home.css'
+import { Link } from 'react-router-dom';
+import './Home.css';
+import boxLogo from '../../images/fs.png';
+import titleLogo from '../../images/title-green.png';
 
 const Home = () => {
-
   return (
-    <div className='home-container'>
-      <div className='top-home-bar'>
-        <img src={boxLogo} alt='small logo' className='small-logo'/>
-        <div className='home-links'>
-          <Link to='/login' className='links log-in' style={{ textDecoration: 'none' }}>Log In</Link>
-          <Link to='signup' className='links sign-up' style={{ textDecoration: 'none' }}>Sign Up</Link>
+    <div className="home-container">
+      <div className="top-home-bar">
+        <img src={boxLogo} alt="small logo" className="small-logo" />
+        <div className="home-links">
+          <Link
+            to="/login"
+            className="links log-in"
+            style={{ textDecoration: 'none' }}
+          >
+            Log In
+          </Link>
+          <Link
+            to="signup"
+            className="links sign-up"
+            style={{ textDecoration: 'none' }}
+          >
+            Sign Up
+          </Link>
         </div>
       </div>
-      <div className='home-line'></div>
-      <div className='main-title'>
-        <img src={titleLogo} alt='future seeds title' className='big-logo'/>
+      <div className="home-line"></div>
+      <div className="main-title">
+        <img src={titleLogo} alt="future seeds title" className="big-logo" />
       </div>
-      <div className='home-line'></div>
-      <div className='bottom-home'>
-        <div className='mission'>
-          <h4 className='mission-quote'>"... this is where the impactful mission statement goes. Oh my, it's SO impactful. Seeds, Journals, etc."</h4>
-          <p className='mission-author'>- Future Seeds</p>
+      <div className="home-line"></div>
+      <div className="bottom-home">
+        <div className="mission">
+          <h4 className="mission-quote">
+            "... this is where the impactful mission statement goes. Oh my, it's
+            SO impactful. Seeds, Journals, etc."
+          </h4>
+          <p className="mission-author">- Future Seeds</p>
         </div>
       </div>
-    </ div>
-  )
-}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
