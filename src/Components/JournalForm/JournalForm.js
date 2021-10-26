421561702a0e7dfe4520b30dcd87647cd2fb41e4
Hayley Dixon <hdixon37@gmail.com>
2021-10-26 10:15:18 -0600

    Add styling to form page for consistancy and header for form

--- src/Components/JournalForm/JournalForm.js
+++ src/Components/JournalForm/JournalForm.js
@@ -7,6 +7,7 @@ const onSubmit = data => console.log(data)
 
   return (
     <div className='journal-form-container'>
+      <h1 className='form-header'>New Journal Form</h1>
       <form className='journal-form' onSubmit={ handleSubmit(onSubmit) }>
         <input placeholder='Date' className='input-date' { ...register('date') } />
         <input placeholder='Description' className='input-description' { ...register('description') } />
